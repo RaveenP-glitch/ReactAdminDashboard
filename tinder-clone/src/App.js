a419146bab30c38b85585398747f37536bf6ef96
Raveen Panditha <55187981+RaveenP-glitch@users.noreply.github.com>
2022-01-27 09:19:07 +0530

    tinder cards

--- tinder-clone/src/App.js
+++ tinder-clone/src/App.js
@@ -1,6 +1,7 @@
 import React from "react";
 import './App.css';
 import Header from "./Header";
+import TinderCards from "./TinderCards";
 
 
 function App() {
@@ -11,7 +12,7 @@ function App() {
       {/* Header */}
         <Header />
       {/* TinderCards */}
-      
+      <TinderCards />
       {/* SwipeButtons */}
 
 
--- tinder-clone/src/TInderCards.js
+++ tinder-clone/src/TInderCards.js
@@ -1,9 +1,32 @@
-import React from 'react';
+import React,{ useState } from 'react';
+import TinderCard from "react-tinder-card";
 import "./TinderCards.css";
 
 
 function TInderCards() {
-  return <div className='tinderCards'></div>;
+
+  const [people,setPeople] = useState([
+    {name: "Elon Musk",
+    url: "",
+  },
+  {
+    name: "Jeff Bezos",
+    url: "",
+  },
+  ]);
+
+
+  return (<div className='tinderCards'>
+    <div className="tinderCards__cardContainer">
+    
+     {people.map((person) => (
+      <h1>{person.name}</h1>
+    ))}
+
+    </div>
+    
+  </div>
+  );
 }
 
 export default TInderCards;
