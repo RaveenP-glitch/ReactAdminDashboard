4d4ebf5e5dff8e3aa94a32e32a116713877fb21d
Raveen Panditha <55187981+RaveenP-glitch@users.noreply.github.com>
2022-01-27 23:55:53 +0530

    swipebuttons

--- tinder-clone/src/App.js
+++ tinder-clone/src/App.js
@@ -1,6 +1,7 @@
 import React from "react";
 import './App.css';
 import Header from "./Header";
+import SwipeButtons from "./SwipeButtons";
 import TinderCards from "./TinderCards";
 
 
@@ -14,7 +15,7 @@ function App() {
       {/* TinderCards */}
       <TinderCards />
       {/* SwipeButtons */}
-
+       <SwipeButtons />
 
     </div>
   );
--- tinder-clone/src/TInderCards.js
+++ tinder-clone/src/TInderCards.js
@@ -3,24 +3,42 @@ import TinderCard from "react-tinder-card";
 import "./TinderCards.css";
 
 
-function TInderCards() {
+function TinderCards() {
 
   const [people,setPeople] = useState([
     {name: "Elon Musk",
-    url: "",
+    url: "https://454438.smushcdn.com/2569731/wp-content/uploads/2012/07/pictures-of-famous-people/mark-zuckerberg.jpg?size=600x500&lossy=1&strip=1&webp=1",
   },
   {
     name: "Jeff Bezos",
-    url: "",
+    url: "https://images.unsplash.com/photo-1541726156-b8aff4dcce65?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MTd8fGZhbW91cyUyMHBlb3BsZXxlbnwwfHwwfHw%3D&auto=format&fit=crop&w=500&q=60",
   },
   ]);
+
+  const swiped = (direction, nameToDelete) =>{
+    console.log("removing: "+nameToDelete);
+  };
+
+  const outOfFrame = (name) => {
+    console.log(name + "left the screen!");
+  };
+
 
 
   return (<div className='tinderCards'>
     <div className="tinderCards__cardContainer">
     
      {people.map((person) => (
-      <h1>{person.name}</h1>
+   
+      <TinderCard
+      className="swipe"
+      key={person.name}
+      preventSwipe={["up","down"]}
+      onSwipe={(dir) => swiped(dir, person.name)}
+      onCardLeftScreen={() => outOfFrame(person.name)}
+      >
+
+      </TinderCard>
     ))}
 
     </div>
@@ -29,4 +47,4 @@ function TInderCards() {
   );
 }
 
-export default TInderCards;
+export default TinderCards;
